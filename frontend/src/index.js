0fd5f18b4370394e1c0aeb30dd9cbb83d8f83647
jostincp <jostincastillop@hotmail.com>
2024-11-10 14:55:13 -0500

    Se solucuiona error de axios crendo un documento xiosConfig.js en srce para tener disponible la configuración de axios en el proyecto

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from './axiosConfig'; // Importa la instancia configurada de Axios
+
+// Añade Axios al ámbito global
+window.axios = axios;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si deseas comenzar a medir el rendimiento en tu aplicación, pasa una función
+// para registrar los resultados (por ejemplo: reportWebVitals(console.log))
+// o envíalos a un endpoint de análisis. Aprende más: https://bit.ly/CRA-vitals
 reportWebVitals();
